3d2cd0c559cf3fe93ab96bdf7af1b48ca18a049c
Matthew Schroeder <dumpypotatoes@gmail.com>
2015-10-08 15:58:54 -0700

    os-lab2

--- kern/startup/main.c
+++ kern/startup/main.c
@@ -101,7 +101,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Matthew Schroeder's system version %s (%s #%d)\n", 
+	kprintf("Matthew Schroeder's awesome system version %s (%s #%d)\n", 
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
